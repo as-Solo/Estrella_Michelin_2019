779fcb40bfef5bb77c11d5c3992e27dd87266b63
as-Solo <asdelsolo@gmail.com>
2021-04-10 17:26:57 +0200

    .

---
+++ src/scraping.py
@@ -0,0 +1,15 @@
+# Estas funciones están pensadas para poder devolver unos datos concretos de la web oficial de Estrella Michelín para aumentar el dataset
+
+
+def scrap_phone():
+    pass
+
+def scrap_mail():
+    pass
+
+def scrap_horario():
+    pass
+
+def scrap_precio():
+    pass
+
